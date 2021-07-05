6b4087a961bccf4189c8984558afbcbfcee254e8
Kevin Rios fuentes <krios-fu@c1r8s2.42madrid.com>
2021-07-05 16:04:30 +0200

    before updtae jacgarci libft lst

--- srcs/prompt/pront.c
+++ srcs/prompt/pront.c
@@ -6,7 +6,7 @@
 /*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 19:25:08 by krios-fu          #+#    #+#             */
-/*   Updated: 2021/07/03 23:56:15 by krios-fu         ###   ########.fr       */
+/*   Updated: 2021/07/05 16:00:43 by krios-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ char	*prompt(void)
 **	see struct libminishell.h
 */
 
+/*
 void	init_parseo(t_data **data, char *argv)
 {
 	char	**line;
@@ -57,7 +58,7 @@ t_data	*set_cmd(char *argv[])
 	t_data	*data;
 
 	data = (t_data *)malloc(sizeof(t_data));
-}
+}*/
 
 int	main(void)
 {
