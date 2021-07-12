4b538d1412facb6aa344aa7a0afff22d52f7bccf
Kevin Rios <rios.kevin.12@gmail.com>
2021-07-12 02:34:33 +0200

    mod error quote, created functions is_quote, check_split...

--- srcs/prompt/expansive_var.c
+++ srcs/prompt/expansive_var.c
@@ -6,7 +6,7 @@
 /*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 23:30:51 by krios-fu          #+#    #+#             */
-/*   Updated: 2021/07/11 05:33:32 by krios-fu         ###   ########.fr       */
+/*   Updated: 2021/07/12 02:28:36 by krios-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,53 @@
 /*
 **	char** (*get_var)(t_list *, char *)
 */
+
+int		is_quote(char c)
+{
+	if (c == '\'' || c == '\"')
+		return (1);
+	return (0);
+}
+
+int		check_split_quote(char *arg)
+{
+	int q_d;
+	int q_s;
+	int i;
+
+	i = 0;
+	q_d = 0;
+	q_s = 0;
+
+	while (arg[i])
+	{
+		if (is_quote(arg[i]))
+		{
+			if(arg[i] == '\'')
+				q_s++;
+			else
+				q_d++;
+		}
+		i++;
+	}
+	if (q_s == 2 || q_d == 2)
+		return (1);
+	return (0);
+}
+
+char	get_quotes_split(char *arg)
+{
+	int i;
+
+	i = 0;
+	while (arg[i])
+	{
+		if (is_quote(arg[i]))
+			return (arg[i]);
+		i++;
+	}
+	return ('\"');
+}
 
 void	del_quotes(t_process *process)
 {
@@ -30,11 +77,11 @@ void	del_quotes(t_process *process)
 	i = 0;
 	j = 0;
 	len_token = 0;
-	while (process->argv[i] != '\0')
+	while (process->argv[i] != (void *)0)
 	{
 		tmp_join = ft_strdup("");
 		j = 0;
-		tmp = ft_split(process->argv[i], '\"');
+		tmp = ft_split(process->argv[i], get_quotes_split(process->argv[i]));
 		while (tmp[j])
 		{
 			aux = tmp_join;
@@ -47,9 +94,11 @@ void	del_quotes(t_process *process)
 		aux = process->argv[i];
 		process->argv[i] = tmp_join;
 		free(aux);
+		i -= check_split_quote(process->argv[i]);
 		i++;
 	}
 }
+
 void	expansive_token(t_process *process)
 {
 	int		i;
@@ -67,34 +116,33 @@ void	expansive_token(t_process *process)
 	while (process->argv[i])
 	{
 		j = 0;
-	if (process->argv[i][j] != '\'')
-		while(process->argv[i][j])
-		{
-			if (process->argv[i][j] == '$')
-				{
-					j++;
-					len_exp = 0;
-					while (process->argv[i][j] && (process->argv[i][j] != ' ' && process->argv[i][j] != '$'))
-						{
-							len_exp++;
-							j++;
-						}
-					j -= len_exp;
-					tmp = ft_strndup("42\0", 2); // val $
-					before_exp = ft_strndup(process->argv[i], j - 1);
-					join_befor_tmp = ft_strjoin(before_exp, tmp);
-					free(tmp);
-					free(before_exp);
-					after_exp = ft_strjoin(join_befor_tmp, &process->argv[i][j + len_exp]);
-					free(join_befor_tmp);
-					free(process->argv[i]);
-					process->argv[i] = after_exp;
-					j--;
-				}
-			j++;
-		}
+		if (process->argv[i][j] != '\'')
+			while(process->argv[i][j])
+			{
+				if (process->argv[i][j] == '$')
+					{
+						j++;
+						len_exp = 0;
+						while (process->argv[i][j] && (process->argv[i][j] != ' '
+							&& process->argv[i][j] != '$' && !is_quote(process->argv[i][j])))
+							{
+								len_exp++;
+								j++;
+							}
+						 j -= len_exp;
+						tmp = ft_strndup("42\0", 2); // val $
+						before_exp = ft_strndup(process->argv[i], j - 1);
+						join_befor_tmp = ft_strjoin(before_exp, tmp);
+						after_exp = ft_strjoin(join_befor_tmp, &process->argv[i][j + len_exp]);
+						free(tmp);
+						free(before_exp);
+						free(join_befor_tmp);
+						free(process->argv[i]);
+						process->argv[i] = after_exp;
+					}
+				j++;
+			}
 		i++;
 	}
-	// printf("[[[%s]]]\n", after_exp);
 	del_quotes(process);
 }
