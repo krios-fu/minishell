d6caedd502bd948d11e24e448af1fdf4ab1c4262
Kevin Rios fuentes <krios-fu@c1r8s2.42madrid.com>
2021-07-13 18:23:52 +0200

    created functions expansive aux

--- srcs/prompt/expansive_var.c
+++ srcs/prompt/expansive_var.c
@@ -6,7 +6,7 @@
 /*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 23:30:51 by krios-fu          #+#    #+#             */
-/*   Updated: 2021/07/13 18:00:48 by krios-fu         ###   ########.fr       */
+/*   Updated: 2021/07/13 18:14:41 by krios-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,54 +99,56 @@ void	del_quotes(t_process *process)
 	}
 }
 
+static void	len_expansive(t_var *var)
+{
+	var->j++;
+	var->len_exp = 0;
+	while (var->token[var->i][var->j] && (var->token[var->i][var->j] != ' '
+		&& var->token[var->i][var->j] != '$' && !is_quote(var->token[var->i][var->j])))
+		{
+			var->len_exp++;
+			var->j++;
+		}
+}
+
+static void	expansive_swap(t_shell *shell, t_var *var)
+{
+	var->j -= var->len_exp;
+	var->env = ft_strndup(&var->token[var->i][var->j], var->len_exp); 
+	var->content = search_env(shell->data->envp_list, var->env);
+	free(var->env);
+	var->before_exp = ft_strndup(var->token[var->i], var->j - 1);
+	var->join_befor_tmp = ft_strjoin(var->before_exp,
+		&var->content[var->len_exp + 1]);
+	var->after_exp = ft_strjoin(var->join_befor_tmp,
+		&var->token[var->i][var->j + var->len_exp]);
+	free(var->content);
+	free(var->before_exp);
+	free(var->join_befor_tmp);
+	free(var->token[var->i]);
+	var->token[var->i] = var->after_exp;
+}
 void	expansive_token(t_shell *shell)
 {
-	int		i;
-	int		j;
-	int		len_exp;
-	char	*content;
-	char	*before_exp;
-	char	*after_exp;
-	char	*join_befor_tmp;
-	char	*env;
-	char	**token;
+	t_var	var;
 
-	i = 0;
-	len_exp = 0;
-	token = shell->data->lst_process->argv;
-	while (token[i])
+	var.i = 0;
+	var.len_exp = 0;
+	var.token = shell->data->lst_process->argv;
+	while (var.token[var.i])
 	{
-
-		j = 0;
-		if (token[i][j] != '\'')
-			while(token[i][j])
+		var.j = 0;
+		if (var.token[var.i][var.j] != '\'')
+			while(var.token[var.i][var.j])
 			{
-				if (token[i][j] == '$')
+				if (var.token[var.i][var.j] == '$')
 					{
-						j++;
-						len_exp = 0;
-						while (token[i][j] && (token[i][j] != ' '
-							&& token[i][j] != '$' && !is_quote(token[i][j])))
-							{
-								len_exp++;
-								j++;
-							}
-						 j -= len_exp;
-						env = ft_strndup(&token[i][j], len_exp); 
-						content = search_env(shell->data->envp_list, env);
-						free(env);
-						before_exp = ft_strndup(token[i], j - 1);
-						join_befor_tmp = ft_strjoin(before_exp, &content[len_exp + 1]);
-						after_exp = ft_strjoin(join_befor_tmp, &token[i][j + len_exp]);
-						free(content);
-						free(before_exp);
-						free(join_befor_tmp);
-						free(token[i]);
-						token[i] = after_exp;
+						len_expansive(&var);
+						expansive_swap(shell, &var);
 					}
-				j++;
+				var.j++;
 			}
-		i++;
+		var.i++;
 	}
 	del_quotes(shell->data->lst_process);
 }
