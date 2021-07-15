87487cca459934e6e82f3fcd294d27b9347b8534
Kevin Rios fuentes <krios-fu@c1r7s2.42madrid.com>
2021-07-15 17:00:18 +0200

    header change

--- srcs/prompt/prompt.c
+++ srcs/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 19:25:08 by krios-fu          #+#    #+#             */
-/*   Updated: 2021/07/15 00:05:53 by krios-fu         ###   ########.fr       */
+/*   Updated: 2021/07/15 16:59:21 by krios-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 
 void	print_header(void)
 {
-	printf(CYAN"::::    ::::  ::::::::::: ::::    ::: :::::::::::  ::::::::  :::    ::: :::::::::: :::        :::\n");
-	printf(CYAN"+:+:+: :+:+:+     :+:     :+:+:   :+:     :+:     :+:    :+: :+:    :+: :+:        :+:        :+:\n");
-	printf(CYAN"+:+ +:+:+ +:+     +:+     :+:+:+  +:+     +:+     +:+        +:+    +:+ +:+        +:+        +:+\n");
-	printf(BLUE"+#+  +:+  +#+     +#+     +#+ +:+ +#+     +#+     +#++:++#++ +#++:++#++ +#++:++#   +#+        +#+\n");
-	printf(BLUE"+#+       +#+     +#+     +#+  +#+#+#     +#+            +#+ +#+    +#+ +#+        +#+        +#+\n");
-	printf(GREEN"#+#       #+#     #+#     #+#   #+#+#     #+#     #+#    #+# #+#    #+# #+#        #+#        #+#\n");
-	printf(GREEN"###       ### ########### ###    #### ###########  ########  ###    ### ########## ########## ##########\n");
-	printf(YELLOW"\t\t\t\t\t\t\t\t\t\tBy: Jacgarci && Krios-fu\n");
+printf(CYAN"                        ✨                              ,dPYb,              ,dPYb, ,dPYb,\n");
+printf(CYAN"✨                                             ✨       IP'`Yb    ✨        IP'`Yb IP'`Yb\n");
+printf(CYAN"           ✨        gg           🪐     gg             I8  8I              I8  8I I8  8I\n");
+printf(CYAN"                     \"\"                  \"\"             I8  8'         ✨   I8  8' I8  8'\n");
+printf(CYAN"  ,ggg,,ggg,,ggg,✨  gg    ,ggg,,ggg,    gg     ,g,  ✨ I8 dPgg,    ,ggg,   I8 dP  I8 dP\n");
+printf(CYAN" ,8\" \"8P\" \"8P\" \"8,   88   ,8\" \"8P\" \"8,   88    ,8\'8,    I8dP\" \"8I  i8\" \"8i  I8dP   I8dP\n");
+printf(CYAN" I8   8I   8I   8I   88   I8   8I   8I   88   ,8\' Yb    I8P    I8  I8, ,8I  I8P    I8P\n");
+printf(CYAN",dP   8I   8I   Yb,_,88,_,dP   8I   Yb,_,88,_,8\'_   8) ,d8  ✨  I8, `YbadP\' ,d8b,_ ,d8b,_\n");
+printf(CYAN"8P'   8I   8I   `Y88P\"\"Y88P'   8I   `Y88P\"\"Y8P\' \"YY8P8P88P     `Y8888P\"Y8888P\'\"Y888P\'\"Y88\n");
+printf(YELLOW"\t\t\t\t\t\t\t\tBy: Jacgarci && Krios-fu\n");
 }
 
 
